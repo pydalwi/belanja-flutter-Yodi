b6205c3f6e38efc26950566d41d9b64223b14ebf
pydalwi <yodialwiono13@gmail.com>
2022-03-23 20:26:57 +0700

    Navigation and Route

---
+++ lib/pages/home_page.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+import 'package:belanja_flutter/pages/item_page.dart';
+import 'package:belanja_flutter/models/item.dart';
+
+class HomePage extends StatelessWidget {
+  final List<Item> items = [
+    Item(name: 'Sugar', price: 5000),
+    Item(name: 'Salt', price: 2000),
+  ];
+
+  HomePage({Key? key}) : super(key: key);
+  // throw UnimplementedError();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Belanja App'),
+      ),
+      body: Container(
+        margin: const EdgeInsets.all(8),
+        child: ListView.builder(
+          padding: const EdgeInsets.all(8),
+          itemCount: items.length,
+          itemBuilder: (context, index) {
+            final item = items[index];
+            return InkWell(
+              onTap: () {
+                Navigator.pushNamed(context, ItemPage.routeName,
+                    arguments: item);
+              },
+              child: Card(
+                child: Container(
+                  margin: const EdgeInsets.all(8),
+                  child: Row(
+                    children: [
+                      Expanded(
+                        child: Text(item.name),
+                      ),
+                      Expanded(
+                        child: Text(
+                          item.price.toString(),
+                          textAlign: TextAlign.end,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/pages/item_page.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:belanja_flutter/models/item.dart';
+
+class ItemPage extends StatelessWidget {
+  const ItemPage({Key? key}) : super(key: key);
+
+  static const routeName = '/extractArguments';
+
+  @override
+  Widget build(BuildContext context) {
+    final Item itemArgs = ModalRoute.of(context)!.settings.arguments as Item;
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Belanja App'),
+      ),
+      body: Center(
+        child: Text(itemArgs.name + ' with' + itemArgs.price.toString()),
+      ),
+    );
+  }
+}
